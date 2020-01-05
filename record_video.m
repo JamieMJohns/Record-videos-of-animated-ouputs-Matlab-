ae843078c17015b8dc8c54391bb0d6267fd66799
Jamie M Johns <34957371+JamieMJohns@users.noreply.github.com>
2020-01-05 13:51:12 +1000

    Update record_video.m

--- record_video.m
+++ record_video.m
@@ -1,10 +1,13 @@
 function writerObj_out=record_video(details,writerObj,action)
 % Code created by Jamie M Johns 2019
 %  Github repository: https://github.com/JamieMJohns/Record-videos-of-animated-ouputs-Matlab-
-%details={fps(double/single),video_title,overwrite(=1=overwrite if same name exist,0=add "_n" (n=int) if file name exist)
-%action= 1 = intialise video (for action 1, writerObj can be anything, for =2 or 3;  writerObj must be created object from action=1)
-%action = 2 = capture frame;
-%action = 3 = stop recording;
+%details={fps(double/single),video_title,overwrite} (overwrite=1= overwrite if same name exist,overwrite=other = add "_n" (n=int) if file name exist)
+%           note that video_title = string and should not include video type (output is always AVI and '.avi' is always added to file name)                           
+%action (if)= 1 = intialise video (for action 1, writerObj can be anything, for =2 or 3;  writerObj must be created object from action=1)
+%action (if)= 2 = capture frame;
+%action (if)= 3 = stop recording;
+%writerObj = input video object (can be anyvalue for action=1)
+%writerObj_out = output video object (created from action=1, for action=2 and 3; input (writerObj) should be that created from action=1)
 % EXAMPLE USAGE (in code) #################################################################################
 %   record_details={30,'my_video',0} %<<<<<<< {fps,title,and overwrite condition(0,1)}
 %   video_object=record_video(record_details,nan,1); %<<<<<<<<<<<< [1] initial video object 
@@ -19,14 +22,13 @@ function writerObj_out=record_video(details,writerObj,action)
 
 
 fps=details{1}; %frames pers second for your video
-recording=1; %=1 if record animated output
 vidname=details{2};  % NAME OF YOUR VIDEO vidname must include ' ' (define vidname as string)
-overwrite=details{3};% if =0 and video with same file name as above; '_new' will added on to file name to avoid overwriting
-                    % else file with same as above (defined by myfirstvideo) will be overwrittern during record
+overwrite=details{3};% overwrite_parameter (=1 equation overwrite video if video of same filename exists (and is avi)
+                     %                     (=0 don't overwrite, create file of same name with suffix "_{integer}"
+                    
+                    
+if action==1 %intialise $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
-                    
-                    
-if action==1 %intialise
                %@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@         
               % check that no file exists with the same name, and modify if exists and overwritevid~=0
                
@@ -45,15 +47,15 @@ if action==1 %intialise
           writerObj0.Quality = 100;   %100 equal 100 percent quality, 0 = 0% quality (100 is best)
           open(writerObj0); %open writer object for recording 
        writerObj_out=writerObj0;
-elseif action==2 %frame capture and add to video
+elseif action==2 %frame capture and add to video$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
     frame = getframe(gcf);
    writeVideo(writerObj,frame);  
    writerObj_out=writerObj;
-elseif action==3 %stop recording
+elseif action==3 %stop recording$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
     close(writerObj);
     fprintf('\nVideo object CLOSED\n The output path of your video is:\n %s\n',pwd) %print, in command window, location of saved video file
     writerObj_out=nan;
-end
+end %$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 
 
